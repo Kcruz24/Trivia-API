2f97c2f327425f8ab15fe9980fd2e887909e3d10
Kevin Cruz <kocruz.24@gmail.com>
2021-08-10 19:48:28 -0400

    Add test for get_categories (Success)

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -37,6 +37,14 @@ class TriviaTestCase(unittest.TestCase):
     # TODO: Write at least one test for each test for successful operation
     #       and for expected errors.
 
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['all_categories'])
+        self.assertTrue(len(data['categories']))
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
