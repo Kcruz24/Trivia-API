0a7b5a9e0fc100b6d5dec46b47299da095cf9d43
Kevin Cruz <kocruz.24@gmail.com>
2021-08-15 21:15:22 -0400

    Add test for search questions when the searchTerm does not exist (Success)

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -137,6 +137,15 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data['questions']))
         self.assertTrue(data['total_questions'])
 
+    def test_if_searchTerm_does_not_exist(self):
+        res = self.client().post('/questions', json={'searchTerm': 'title'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['questions'], [])
+        self.assertEqual(len(data['questions']), 0)
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
